ca53a742878fe88caca05c33a62f00cbc09991c0
5OO <git.hub@myrk.eu>
2024-05-12 00:50:24 +0300

    Update ParticipantsOfEventView layout and styles
    This commit updates the layout and styles of the ParticipantsOfEventView. The redesign includes changes done to the events and participants details section for improved readability. Also, the form layout has been refined for individual and company participants, and the styles for elements have been updated for a better look and feel.

--- src/views/ParticipantsOfEventView.vue
+++ src/views/ParticipantsOfEventView.vue
@@ -118,163 +118,188 @@ onMounted(async () => {
   await getPaymentMethodsList();
 });
 </script>
-
 <template>
-  <div>
-    <h1>Participants</h1>
-    <h2>Event Details</h2>
-    <ul>
-      <li>Event name: {{ eventDetails?.eventName }}</li>
-      <li>Event date: {{ eventDetails?.eventDate }}</li>
-      <li>Location: {{ eventDetails?.location }}</li>
-    </ul>
-
-    <h2>Participants List</h2>
-    <table>
-      <thead>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Code/ID</th>
-        <th>Action</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="(participant, index) in participants" :key="participant.participantId">
-        <td>{{ index + 1 }}</td>
-        <td>{{ participant.name }}</td>
-        <td>{{ participant.codeOrId }}</td>
-        <td>
-          <button type="button" class="back-btn" @click="viewParticipantDetails(participant)">View</button>
-          <button type="submit" class="update-btn" @click="deleteParticipant(participant)">Delete</button>
-
-        </td>
-      </tr>
-      </tbody>
-    </table>
-
-    <h2>Add Participant</h2>
-    <div class="participant-type-selection">
-      <input
-        type="radio"
-        id="individual"
-        value="individual"
-        v-model="selectedParticipantType"
-      />
-      <label for="individual">Individual</label>
-      <input
-        type="radio"
-        id="company"
-        value="company"
-        v-model="selectedParticipantType"
-      />
-      <label for="company">Company</label>
+  <div class="container my-5">
+    <!-- Header Bar with Text and Image -->
+    <div class="d-flex align-items-stretch">
+      <h1 class="flex-grow-1 mb-0">Participants</h1>
+      <div class="image-container">
+        <img src="@/assets/center-img-labeled.jpg" alt="Event">
+      </div>
     </div>
 
 
-    <form @submit.prevent="addParticipant">
-      <div v-if="selectedParticipantType === 'individual'">
-        <div class="form-group">
-          <label for="firstName">First Name:</label>
-          <input type="text" id="firstName" v-model="individualForm.firstName" required />
-        </div>
-        <div class="form-group">
-          <label for="lastName">Last Name:</label>
-          <input type="text" id="lastName" v-model="individualForm.lastName" required />
-        </div>
-        <div class="form-group">
-          <label for="personalID">Personal ID:</label>
-          <input type="text" id="personalID" v-model="individualForm.personalID" required />
-        </div>
-        <div class="form-group">
-          <label for="paymentMethod">Payment Method:</label>
-          <select id="paymentMethod" v-model="individualForm.paymentMethod" required>
-            <option value="">Select</option>
-            <option v-for="method in paymentMethods" :key="method" :value="method">{{ method }}</option>
-          </select>
-        </div>
-        <div class="form-group">
-          <label for="additionalInfo">Additional Info:</label>
-          <textarea id="additionalInfo" v-model="individualForm.additionalInfo"></textarea>
-        </div>
+    <!-- Event Details -->
+    <div class="mb-4">
+      <ul class="list-group">
+        <li class="list-group-item">Event name: {{ eventDetails?.eventName }}</li>
+        <li class="list-group-item">Event date: {{ eventDetails?.eventDate }}</li>
+        <li class="list-group-item">Location: {{ eventDetails?.location }}</li>
+      </ul>
+    </div>
+
+    <!-- Participants List -->
+    <div class="mb-4">
+      <table class="table">
+        <thead>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Code/ID</th>
+          <th>Action</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(participant, index) in participants" :key="participant.participantId">
+          <td>{{ index + 1 }}</td>
+          <td>{{ participant.name }}</td>
+          <td>{{ participant.codeOrId }}</td>
+          <td>
+              <span class="text-dark cursor-pointer" @click="viewParticipantDetails(participant)" role="button">View</span>
+              <span class="text-dark cursor-pointer" @click="deleteParticipant(participant)" role="button">Delete</span>
+
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <!-- Add Participant Form -->
+    <div class="mb-4">
+
+
+      <h2>Add Participant</h2>
+      <div class="participant-type-selection">
+        <input
+          type="radio"
+          id="individual"
+          value="individual"
+          v-model="selectedParticipantType"
+        />
+        <label for="individual">Individual</label>
+        <input
+          type="radio"
+          id="company"
+          value="company"
+          v-model="selectedParticipantType"
+        />
+        <label for="company">Company</label>
       </div>
 
-      <div v-else-if="selectedParticipantType === 'company'">
-        <div class="form-group">
-          <label for="legalName">Legal Name:</label>
-          <input type="text" id="legalName" v-model="companyForm.legalName" required />
-        </div>
-        <div class="form-group">
-          <label for="registrationCode">Registration Code:</label>
-          <input type="text" id="registrationCode" v-model="companyForm.registrationCode" required />
-        </div>
-        <div class="form-group">
-          <label for="numberOfParticipants">Number of Participants:</label>
-          <input type="number" id="numberOfParticipants" v-model="companyForm.numberOfParticipants" required />
-        </div>
-        <div class="form-group">
-          <label for="paymentMethod">Payment Method:</label>
-          <select id="paymentMethod" v-model="companyForm.paymentMethod" required>
-            <option value="">Select</option>
-            <option v-for="method in paymentMethods" :key="method" :value="method">{{ method }}</option>
-          </select>
-        </div>
-        <div class="form-group">
-          <label for="additionalInfo">Additional Info:</label>
-          <textarea id="additionalInfo" v-model="companyForm.additionalInfo"></textarea>
-        </div>
-      </div>
 
-      <div class="form-actions">
-        <button type="button" class="back-btn" @click="router.push('/')">Back</button>
-        <button type="submit" class="add-btn">Add Participant</button>
-      </div>
-    </form>
+      <form @submit.prevent="addParticipant">
 
+        <div v-if="selectedParticipantType === 'individual'">
+          <div class="row form-group">
+            <label for="firstName" class="col-sm-2 col-form-label">First Name:</label>
+            <div class="col-sm-4">
+              <input type="text" class="form-control" id="firstName" v-model="individualForm.firstName" required />
+            </div>
+          </div>
+          <div class="row form-group">
+            <label for="lastName" class="col-sm-2 col-form-label">Last Name:</label>
+            <div class="col-sm-4">
+              <input type="text" class="form-control" id="lastName" v-model="individualForm.lastName" required />
+            </div>
+          </div>
+          <div class="row form-group">
+            <label for="personalID" class="col-sm-2 col-form-label">Personal ID:</label>
+            <div class="col-sm-4">
+              <input type="text" class="form-control" id="personalID" v-model="individualForm.personalID" required />
+            </div>
+          </div>
+          <div class="row form-group">
+            <label for="paymentMethod" class="col-sm-2 col-form-label">Payment Method:</label>
+            <div class="col-sm-4">
+              <select class="form-control" id="paymentMethod" v-model="individualForm.paymentMethod" required>
+                <option value="">Select</option>
+                <option v-for="method in paymentMethods" :key="method" :value="method">{{ method }}</option>
+              </select>
+            </div>
+          </div>
+          <div class="row form-group">
+            <label for="additionalInfo" class="col-sm-2 col-form-label">Additional Info:</label>
+            <div class="col-sm-4">
+              <textarea class="form-control" id="additionalInfo" v-model="individualForm.additionalInfo"></textarea>
+            </div>
+          </div>
+        </div>
+
+        <div v-else-if="selectedParticipantType === 'company'">
+          <div class="row form-group">
+            <label for="legalName" class="col-sm-2 col-form-label">Legal Name:</label>
+            <div class="col-sm-4">
+              <input type="text" class="form-control" id="legalName" v-model="companyForm.legalName" required />
+            </div>
+          </div>
+          <div class="row form-group">
+            <label for="registrationCode" class="col-sm-2 col-form-label">Registration Code:</label>
+            <div class="col-sm-4">
+              <input type="text" class="form-control" id="registrationCode" v-model="companyForm.registrationCode" required />
+            </div>
+          </div>
+          <div class="row form-group">
+            <label for="numberOfParticipants" class="col-sm-2 col-form-label">Number of Participants:</label>
+            <div class="col-sm-4">
+              <input type="number" class="form-control" id="numberOfParticipants" v-model="companyForm.numberOfParticipants" required />
+            </div>
+          </div>
+          <div class="row form-group">
+            <label for="paymentMethod" class="col-sm-2 col-form-label">Payment Method:</label>
+            <div class="col-sm-4">
+              <select class="form-control" id="paymentMethod" v-model="companyForm.paymentMethod" required>
+                <option value="">Select</option>
+                <option v-for="method in paymentMethods" :key="method" :value="method">{{ method }}</option>
+              </select>
+            </div>
+          </div>
+          <div class="row form-group">
+            <label for="additionalInfo" class="col-sm-2 col-form-label">Additional Info:</label>
+            <div class="col-sm-4">
+              <textarea class="form-control" id="additionalInfo" v-model="companyForm.additionalInfo"></textarea>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="form-actions">
+          <button type="button" class="back-btn" @click="router.push('/')">Back</button>
+          <button type="submit" class="add-btn">Add Participant</button>
+        </div>
+      </form>
+
+    </div>
   </div>
 </template>
 
-<style scoped>
+
+<style>
+
 table {
   width: 100%;
   border-collapse: collapse;
 }
 
-th,
-td {
-  border: 1px solid #ddd;
-  padding: 8px;
-}
 
-th {
-  background-color: #f4f4f4;
-  text-align: left;
+.container{
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
 }
-
-span {
-  cursor: pointer;
-  margin-right: 10px;
-  color: blue;
-  text-decoration: underline;
-}
-
-button {
-  margin-top: 20px;
-  padding: 10px 20px;
-  cursor: pointer;
+.image-container {
+  width: auto;
+  max-width: 100%;
+  overflow: hidden;
   background-color: #005aa1;
   color: white;
-  border: none;
-  border-radius: 4px;
 }
 
-button:hover {
-  opacity: 0.9;
-}
-
-h2 {
-  margin-top: 30px;
-  color: #005aa1;
+.image-container img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover; /* This is the key! */
+  background-color: #005aa1;
+  color: white;
 }
 
 .form-group {
@@ -295,7 +320,6 @@ select {
   border: 1px solid #ccc;
   border-radius: 4px;
 }
-
 
 .form-actions {
   display: flex;
@@ -332,5 +356,21 @@ select {
   gap: 20px; /* Adjust spacing between radio buttons as needed */
   align-items: center;
 }
+
+.cursor-pointer {
+  cursor: pointer;
+  padding: 10px;
+  color: black;
+text-transform: uppercase;
+}
+
+
+.text-dark.cursor-pointer:hover {
+  color: red; /* Example of a neutral gray hover color for the View link */
+}
+.cursor-pointer:hover {
+  text-decoration: underline;
+}
+
 
 </style>
